8c9bd892bfd324734c695059e47fb91294317712
Alireza Moradi <162599027+AlirezaMoradiDev@users.noreply.github.com>
2026-09-05 16:24:28 +0330

    Time: 2 ms (29.26%), Space: 30.2 MB (94.5%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        for(int i = 0; i < nums.size(); i++){
+            if(*max_element(nums.begin() + 0, nums.begin() + i + 1) - *min_element(nums.begin() + i, nums.end()) <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
